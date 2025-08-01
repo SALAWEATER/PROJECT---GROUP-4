dfed3075b3495d7ecda7fa48b70ffcb1e83abe84
holgadoa <holgadoave@gmail.com>
2025-08-01 21:08:25 +0800

    removed the old source code folder

--- mentalhealth-app/data/init_db.py
+++
@@ -1,7 +0,0 @@
-# init_db.py
-from data.database import engine
-from business.models import Base  # Import models to register them with SQLAlchemy
-
-print("Creating database tables...")
-Base.metadata.create_all(bind=engine)
-print("Tables created successfully!")
